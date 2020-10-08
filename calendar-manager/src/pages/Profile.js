4ff0fdbd25e60f328a28ec294e73a37b1f455da6
Shiyue Zhang <shiyuezhang@Shiyues-MacBook-Pro.local>
2020-10-08 15:42:53 -0500

    modify profile page to show classes

--- calendar-manager/src/pages/Profile.js
+++ calendar-manager/src/pages/Profile.js
@@ -6,6 +6,8 @@ import {
   useFirestoreDocData,
 } from 'reactfire';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import {makeStyles} from '@material-ui/core/styles';
 import {useParams, useHistory} from 'react-router-dom';
 import Navbar from '../common/Navbar';
@@ -14,6 +16,9 @@ const useStyles = makeStyles((theme) => ({
   email: {
     color: 'purple',
   },
+  classCodes: {
+
+  }
 }));
 
 /**
@@ -25,14 +30,42 @@ export default function Profile() {
 //   const [currUser, setCurrUser] = useState(useUser());
   const classes = useStyles();
   const {uid} = useParams();
-//   const userCollection = useFirestore().collection('users');
+  // const classCollection = useFirestore().collection('classes');
+  const userData = useFirestoreDocData(useFirestore().collection('users').doc(uid));
+  // console.log(userData);
 
   return (
     <div>
-        <Navbar />
-        <Typography variant="h6" className={classes.email}>
-            {user.email}
-        </Typography>
+      <Navbar />
+      <Box
+        container='true'
+        m={10}
+      >
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Typography variant="h6" className={classes.email}>
+                email: {user.email}
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h5">
+                Classes:
+            </Typography>
+          </Grid>
+          {userData.classCodes.length > 0 ?
+          userData.classCodes.map((v, index) => {
+              return (
+                <Grid item
+                  key={index}
+                  className={classes.classCodes}
+                >
+                    {v.name}
+                </Grid>
+              );
+            }) : <Grid item>No classes added yet</Grid>
+          }
+        </Grid>
+      </Box>
     </div>
   );
 }
