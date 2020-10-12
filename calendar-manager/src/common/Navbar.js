96bc8b4c2e410a255b5d7e1b076e7803d1a19439
cdalangin <florenzdalangin@gmail.com>
2020-10-12 16:33:23 -0400

    put profile and logout in one place

--- calendar-manager/src/common/Navbar.js
+++ calendar-manager/src/common/Navbar.js
@@ -4,7 +4,7 @@ import {
   Toolbar,
   Typography,
   Button,
-  IconButton,
+  IconButton, Menu, MenuItem
 } from "@material-ui/core";
 import { AccountCircle } from "@material-ui/icons";
 import { useHistory } from "react-router-dom";
@@ -12,6 +12,16 @@ import { useUser } from "reactfire";
 import * as firebase from "firebase";
 
 export default function Navbar() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   const history = useHistory();
   const user = useUser();
 
@@ -37,21 +47,99 @@ export default function Navbar() {
     >
       <Toolbar>
         <Typography variant="h6" style={{ flexGrow: 1 }}></Typography>
-        <Button onClick={() => logout()} color="inherit">
-          Log Out
-        </Button>
         <Button href={"/myclasses/" + user.uid} color="inherit">
           My Classrooms
         </Button>
         <IconButton
           aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
+          aria-controls="simple-menu"
+          aria-haspopup="true"
           color="inherit"
-          onClick={() => history.push("/profile/" + user.uid)}
+          onClick={handleClick}
         >
           <AccountCircle />
         </IconButton>
+        <Menu
+          id="simple-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleClose}>
+          <MenuItem onClick={() => history.push("/profile/" + user.uid)}>Profile</MenuItem>
+          <MenuItem onClick={() => logout()}>Logout</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
 }
+
+
+
+// import React from "react";
+// import {
+//   AppBar,
+//   Toolbar,
+//   Typography,
+//   Button,
+//   IconButton, Menu, MenuItem
+// } from "@material-ui/core";
+// import { AccountCircle } from "@material-ui/icons";
+// import { useHistory } from "react-router-dom";
+// import { useUser } from "reactfire";
+// import * as firebase from "firebase";
+
+// export default function Navbar() {
+//   const [anchorEl, setAnchorEl] = React.useState(null);
+
+//   const handleClick = (event) => {
+//     setAnchorEl(event.currentTarget);
+//   };
+
+//   const handleClose = () => {
+//     setAnchorEl(null);
+//   };
+
+//   const history = useHistory();
+//   const user = useUser();
+
+//   if (!user) {
+//     return <></>;
+//   }
+
+//   // logs the user out
+//   const logout = () => {
+//     firebase
+//       .auth()
+//       .signOut()
+//       .then((res) => history.push("/login"))
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+//   };
+
+//   return (
+//     <AppBar
+//       style={{ background: "transparent", boxShadow: "none" }}
+//       position="absolute"
+//     >
+//       <Toolbar>
+//         <Typography variant="h6" style={{ flexGrow: 1 }}></Typography>
+//         <Button onClick={() => logout()} color="inherit">
+//           Log Out
+//         </Button>
+//         <Button href={"/myclasses/" + user.uid} color="inherit">
+//           My Classrooms
+//         </Button>
+//         <IconButton
+//           aria-label="account of current user"
+//           aria-controls="primary-search-account-menu"
+//           color="inherit"
+//           onClick={() => history.push("/profile/" + user.uid)}
+//         >
+//           <AccountCircle />
+//         </IconButton>
+//       </Toolbar>
+//     </AppBar>
+//   );
+// }
+
